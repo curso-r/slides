a58871f34d685d161f27b2a49cd43af2c0352d6d
jtrecenti <julio.trecenti@gmail.com>
2017-05-18 17:34:17 -0300

    aula 8

---
+++ 201704_imejr/aula8/app/app.R
@@ -0,0 +1,34 @@
+library(shiny)
+library(ggplot2)
+library(plotly)
+
+ui <- fluidPage(
+   titlePanel("Old Faithful Geyser Data"),
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 30)
+      ),
+      mainPanel(
+         plotlyOutput("distPlot")
+      )
+   )
+)
+
+server <- function(input, output) {
+  
+   output$distPlot <- renderPlotly({
+      p <- ggplot(faithful) +
+        geom_histogram(aes(x = waiting),
+                       bins = input$bins,
+                       fill = 'white', 
+                       colour = 'black')
+      ggplotly(p)
+   })
+}
+
+shinyApp(ui = ui, server = server)
+
